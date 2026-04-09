385c344906b4cda6bde7e12b31852cd853473be2
aymanaarab <soquavo@gmail.com>
2026-04-09 22:27:31 +0000

    fix

--- docker-start.sh
+++ docker-start.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+# DEBUG
+echo "=== PORT value is: $PORT ==="
+echo "=== Nginx config BEFORE substitution ==="
+cat /etc/nginx/http.d/default.conf
+
 # Substitute $PORT into nginx config
 envsubst '${PORT}' < /etc/nginx/http.d/default.conf > /etc/nginx/http.d/default.conf.tmp
 mv /etc/nginx/http.d/default.conf.tmp /etc/nginx/http.d/default.conf
+
+echo "=== Nginx config AFTER substitution ==="
+cat /etc/nginx/http.d/default.conf
 
 # Run migrations and cache
 php artisan migrate --force
